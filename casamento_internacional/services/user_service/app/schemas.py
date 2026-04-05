74c74c59e81fc23229b2ffe1091e7230a73f3d81
fernandodevpythonn <fernandoribeiro270806@gmail.com>
2026-04-05 14:32:46 -0300

    terminando user_service

---
+++ casamento_internacional/services/user_service/app/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr, constr
+from typing import Optional
+
+class UserBase(BaseModel):
+  nome: str
+  cpf: str
+  email = EmailStr
+  id_perfil: int
+
+class UserCreate(UserBase):
+  senha: str
+
+class UserUpdate(BaseModel):
+  nome: Optional[str]
+  cpf: Optional[str]
+  email: Optional[EmailStr]
+  id_perfil: Optional[int]
+
+class UserOut(UserBase):
+  id_usuario: int
